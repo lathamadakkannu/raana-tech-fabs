4fbadb78f13a17862fd75e664d0ecb4fd0100eda
M.Latha <111737966+lathamadakkannu@users.noreply.github.com>
2024-09-13 17:11:53 +0530

    Update page.js

--- app/Process/page.js
+++ app/Process/page.js
@@ -11,68 +11,47 @@ const Process = () => {
       </h2>
 
       <div className={styles.processContainer}>
-     
         <div className={styles.processRow}>
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>Enquiry From Customer</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>Enquiry From Customer</p>
+            <h5 className={styles.processCount}>1</h5>
           </div>
 
-          <div className={styles.processArrow}>➡</div>
-
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>Estimation, Manufacturing Process Design</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>Estimation, Manufacturing Process Design</p>
+            <h5 className={styles.processCount}>2</h5>
           </div>
 
-          <div className={styles.processArrow}>➡</div>
-
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>Quotation</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>Quotation</p>
+            <h5 className={styles.processCount}>3</h5>
           </div>
 
-          <div className={styles.processArrow}>➡</div>
-
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>Sample Approval</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>Sample Approval</p>
+            <h5 className={styles.processCount}>4</h5>
           </div>
         </div>
 
         <div className={styles.processRow}>
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>JIG, Fixture/Design</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>JIG, Fixture/Design</p>
+            <h5 className={styles.processCount}>5</h5>
           </div>
 
-          <div className={styles.processArrow}>➡</div>
-
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>Production</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>Production</p>
+            <h5 className={styles.processCount}>6</h5>
           </div>
 
-          <div className={styles.processArrow}>➡</div>
-
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>Quality</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>Quality</p>
+            <h5 className={styles.processCount}>7</h5>
           </div>
 
-          <div className={styles.processArrow}>➡</div>
-
-          <div className={styles.processCol}>
-            <div className={styles.processCard}>
-              <p>Dispatch</p>
-            </div>
+          <div className={styles.processCard}>
+            <p>Dispatch</p>
+            <h5 className={styles.processCount}>8</h5>
           </div>
         </div>
       </div>
